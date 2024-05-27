3baeec661f0c8afbb5bf4f9d9917eb2eb60481f8
Kazuki Kijima <kazukiyoshida96@gmail.com>
2024-05-27 16:01:07 -0700

    Add comments

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// Error in dependencies
+// TODO: Configure GitHub Actions to run tests
+test('renders daily report link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/nippo.huyfififi.com/i);
   expect(linkElement).toBeInTheDocument();
 });
