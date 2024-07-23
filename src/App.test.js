164f24389951fdcef4d0423ff7a63a459ef97066
Kazuki Kijima <kazukiyoshida96@gmail.com>
2024-07-22 18:38:31 -0700

    Add a testing workflow

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-// Error in dependencies
-// TODO: Configure GitHub Actions to run tests
 test('renders daily report link', () => {
+  // Warning: `ReactDOMTestUtils.act` is deprecated in favor of `React.act`.
+  // Import `act` from `react` instead of `react-dom/test-utils`.
+  // See https://react.dev/warnings/react-dom-test-utils for more info.
   render(<App />);
   const linkElement = screen.getByText(/nippo.huyfififi.com/i);
   expect(linkElement).toBeInTheDocument();
